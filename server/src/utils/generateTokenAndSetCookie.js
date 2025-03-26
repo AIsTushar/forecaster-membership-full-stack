6f84eaf2f476f6108b347c4e61d0ddd135cc1f63
Azizul Islam <azizultushar98@gmail.com>
2025-03-26 19:16:38 +0600

    refactor token generation

--- server/src/utils/generateTokenAndSetCookie.js
+++ server/src/utils/generateTokenAndSetCookie.js
@@ -1,20 +1,25 @@
 import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, userId) => {
-  // Create Token
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   console.log(token);
 
-  // Set Cookie
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    domain: "vercel.app",
-    path: "/",
-  });
+  if (process.env.NODE_ENV === "production") {
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      domain: "vercel.app",
+      path: "/",
+    });
+  } else {
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  }
 };
