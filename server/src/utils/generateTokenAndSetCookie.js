b23977a8651720f1fbc859acfade49fda692b6ca
Azizul Islam <azizultushar98@gmail.com>
2025-03-26 19:10:27 +0600

    domain

--- server/src/utils/generateTokenAndSetCookie.js
+++ server/src/utils/generateTokenAndSetCookie.js
@@ -6,11 +6,15 @@ export const generateTokenAndSetCookie = (res, userId) => {
     expiresIn: "7d",
   });
 
+  console.log(token);
+
   // Set Cookie
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
     sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
+    domain: "vercel.app",
+    path: "/",
   });
 };
